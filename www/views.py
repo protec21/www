1601a52e25b7af4971ef71cda52627af52244609
leekh <leekh529@gmail.com>
2021-03-04 10:23:09 +0900

    none

--- www/views.py
+++ www/views.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
-import re
-import time
 
-# Create your views here.
 def index(request):
     stock=finance()
     return render(request, 'www/index.html', {'stock': stock})
@@ -28,8 +24,6 @@ def stock(request):
 def product(request):
     return render(request, 'www/product.html', {'message': 'product'})
 
-
-
 def finance():
     url = 'https://finance.naver.com/item/main.nhn?code=053610'
     response = requests.get(url)
@@ -39,7 +33,6 @@ def finance():
     _nowVal = soup.select_one('#content > div.section.trade_compare > table > tbody > tr:nth-child(1) > td:nth-child(2)')
     _nowVal = _nowVal.text
     print(_nowVal)
-
     url = 'https://finance.naver.com/sise'
     response = requests.get(url)
     response.raise_for_status()
@@ -51,6 +44,5 @@ def finance():
     _kosdaq = soup.select_one('#KOSDAQ_now')
     _kosdaq = _kosdaq.text
     print(_kosdaq)
-
     stock = {'nowVal': _nowVal, 'kosdaq': _kosdaq, 'kospi': _kospi}
     return stock
