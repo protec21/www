049b8b8752641eb58c0f982bbafa3f59130f4fc7
leekh <leekh529@gmail.com>
2021-03-10 15:38:08 +0900

    none

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,7 @@ import string, random
 chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
 SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
